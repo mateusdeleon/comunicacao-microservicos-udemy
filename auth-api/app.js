6b604bb554b08529b602870d765fbba01a05b1b8
Mateus De Leon <mateusdeleon1987@gmail.com>
2024-04-23 09:58:57 -0300

    Criação do micro serviço sales-api, implmentação do serviço de autenticação, auth-api.

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { createInitialData } from "./src/config/db/initialData.js";
+import userRoutes from "./src/modules/user/routes/UserRoutes.js"
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+
+createInitialData();
 
 app.get('/api/status', (req,res) => {
     return res
@@ -13,6 +17,9 @@ app.get('/api/status', (req,res) => {
             httpStatus: 200,
         })
 })
+
+app.use(express.json());
+app.use(userRoutes);
 
 app.listen(PORT, () => {
     console.info(`Server started sucessfully at PORT ${PORT} `)
